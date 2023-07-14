7b58840528cf92499856dfd83aa3a115707eb2a6
Kumarion <121711454+Kumarion@users.noreply.github.com>
2023-07-14 12:19:00 -0400

    D

--- src/StarterPlayer/StarterPlayerScripts/TestTextEffects.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/TestTextEffects.client.lua
@@ -1,23 +1,21 @@
-local repS = game:GetService("ReplicatedStorage")
+local TextEffects = require(game:GetService("ReplicatedStorage").TextEffects);
+local PlayerGui = game.Players.LocalPlayer:WaitForChild("PlayerGui");
+local MainUI = PlayerGui:WaitForChild("MainUI");
 
-local TextEffects = require(repS.TextEffects)
-local playerGui = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-local mainUI = playerGui:WaitForChild("MainUI")
+local effect = TextEffects.new(MainUI.Frame1, "Rainbow");
+local effect2 = TextEffects.new(MainUI.Frame2, "Lava");
+local effect3 = TextEffects.new(MainUI.Frame3, "Bubblegum");
 
-local effect = TextEffects.new(mainUI.Frame1, "Rainbow")
-local effect2 = TextEffects.new(mainUI.Frame2, "Lava")
-local effect3 = TextEffects.new(mainUI.Frame3, "Bubblegum")
-
-local textEffect = TextEffects.new(mainUI.TextLabel1, "RainbowStroke", 0.01, 6)
-local textEffect2 = TextEffects.new(mainUI.TextLabel2, "LavaStroke", 0.004, 5)
-local textEffect3 = TextEffects.new(mainUI.TextLabel3, "Ghost", 0.004, 5)
-local textEffect4 = TextEffects.new(mainUI.TextLabel4, "GhostStroke", 0.004, 2)
-local textEffect5 = TextEffects.new(mainUI.TextLabel5, "Gold", 0.009, 2)
-local textEffect6 = TextEffects.new(mainUI.TextLabel6, "GoldStroke", 0.012, 3)
-local textEffect7 = TextEffects.new(mainUI.TextLabel7, "SilverStroke", 0.012, 3)
-local textEffect8 = TextEffects.new(mainUI.TextLabel8, "ChromeStroke", 0.004, 3)
-local textEffect9 = TextEffects.new(mainUI.TextLabel9, "Bubblegum", 0.008, 3)
-local textEffect10 = TextEffects.new(mainUI.TextLabel10, "OceanicStroke", 0.001, 2)
+local textEffect = TextEffects.new(MainUI.TextLabel1, "RainbowStroke", 0.01, 6);
+local textEffect2 = TextEffects.new(MainUI.TextLabel2, "LavaStroke", 0.004, 5);
+local textEffect3 = TextEffects.new(MainUI.TextLabel3, "Ghost", 0.004, 5);
+local textEffect4 = TextEffects.new(MainUI.TextLabel4, "GhostStroke", 0.004, 2);
+local textEffect5 = TextEffects.new(MainUI.TextLabel5, "Gold", 0.009, 2);
+local textEffect6 = TextEffects.new(MainUI.TextLabel6, "GoldStroke", 0.012, 3);
+local textEffect7 = TextEffects.new(MainUI.TextLabel7, "SilverStroke", 0.012, 3);
+local textEffect8 = TextEffects.new(MainUI.TextLabel8, "ChromeStroke", 0.004, 3);
+local textEffect9 = TextEffects.new(MainUI.TextLabel9, "Bubblegum", 0.008, 3);
+local textEffect10 = TextEffects.new(MainUI.TextLabel10, "OceanicStroke", 0.001, 2);
 
 -- task.delay(5, function()
 -- 	print("bubblegum destroy");
