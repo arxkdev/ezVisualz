6db8716f1c7337bd5e95472f791b6055cc63c1f4
Kumarion <121711454+Kumarion@users.noreply.github.com>
2023-07-19 13:59:38 -0400

    Added wavey preset

--- src/StarterPlayer/StarterPlayerScripts/TestTextEffects.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/TestTextEffects.client.lua
@@ -23,6 +23,7 @@ local textEffect13 = EasyVisuals.new(MainUI.Texts.TextLabel13, "IceStroke", 0.00
 local textEffect14 = EasyVisuals.new(MainUI.Texts.TextLabel14, "FireStroke", 0.007, 5);
 local textEffect15 = EasyVisuals.new(MainUI.Texts.TextLabel15, "Matrix", 0.009, 3);
 local textEffect16 = EasyVisuals.new(MainUI.Texts.TextLabel16, "Shine", 0.015, 3, false, Color3.fromRGB(187, 0, 255));
+local textEffect17 = EasyVisuals.new(MainUI.Texts.TextLabel17, "WaveStroke", 0.008, 6, false, Color3.fromRGB(0, 247, 255));
 
 -- task.delay(5, function()
 -- 	print("bubblegum destroy");
